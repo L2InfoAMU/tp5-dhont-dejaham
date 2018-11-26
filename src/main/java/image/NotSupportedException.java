fc20fa864faec4fff811f24b94388b8caf8b516b
FloFloDht <flo.11.italia@gmail.com>
2018-11-26 17:24:30 +0100

    Created NotSupportedException

---
+++ src/main/java/image/NotSupportedException.java
@@ -0,0 +1,8 @@
+package image;
+
+public class NotSupportedException extends RuntimeException {
+    String string;
+    public NotSupportedException(String string){
+        this.string = string;
+    }
+}
