a81f1af89cc7eacb048acd9c7fd4153dc8902415
Charles-DJM <charlesdjfc@gmail.com>
2018-12-03 15:40:10 +0100

    PaletteRasterImage

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -16,4 +16,39 @@ public class PaletteRasterImage implements Image {
         this.width = width;
 
     }
+
+    public PaletteRasterImage(Color[][] pixels){
+
+    }
+
+    public void createRepresentation(){
+
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setPixelColor(Color color, int x, int y){
+
+    }
 }
